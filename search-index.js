8d6b54628cb5f4bdec723ef7527f7c9c0ecace0e
Ancient123 <Ancient123@users.noreply.github.com>
2022-03-03 00:30:03 +0000

    deploy: 185534496805974d3ace4d3d05bdab3753d2c20d

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"zerok_macros":{"doc":"","t":[23,23],"n":["generic_tests","ser_test"],"q":["zerok_macros",""],"d":["Define a set of generic tests which can be instantiated …","Generate round-trip serialization tests."],"i":[0,0],"f":[null,null],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
