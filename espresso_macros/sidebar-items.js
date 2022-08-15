2665800243a3a8a27ea2751529edb6d61a0f16ab
Ancient123 <Ancient123@users.noreply.github.com>
2022-08-15 00:27:06 +0000

    deploy: 9e0b0258c99757c567676c4dd92f1501db0cf580

--- espresso_macros/sidebar-items.js
+++ espresso_macros/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"attr":[["generic_tests","Define a set of generic tests which can be instantiated with different type parameters."],["ser_test","Generate round-trip serialization tests."]]});
+window.SIDEBAR_ITEMS = {"attr":[["generic_tests","Define a set of generic tests which can be instantiated with different type parameters."],["ser_test","Generate round-trip serialization tests."]]};
--- search-index.js
+++ search-index.js
@@ -1,4 +1,5 @@
 var searchIndex = JSON.parse('{\
 "espresso_macros":{"doc":"","t":[23,23],"n":["generic_tests","ser_test"],"q":["espresso_macros",""],"d":["Define a set of generic tests which can be instantiated …","Generate round-trip serialization tests."],"i":[0,0],"f":[null,null],"p":[]}\
 }');
-if (window.initSearch) {window.initSearch(searchIndex)};
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
